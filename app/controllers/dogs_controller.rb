cf3b186c89da8529d6e950464dc736a8da59e416
lgm527 <laurellmccaffrey@gmail.com>
2019-07-24 15:40:07 -0400

    dunder mifflin with dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,38 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only: [:show, :create, :update, :destroy]
+
+  def index
+    @dogs = Dog.all.sort_by { |d| d.employees.count }.reverse
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
+
 end
