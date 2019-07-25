e521001c15321be975031d8512304324978cfec7
Rashad <rashad.walcott15@gmail.com>
2019-07-25 09:17:14 -0400

    Dunder Mifflin Complete

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,39 @@
 class DogsController < ApplicationController
+    before_action :find_dog, only: [:show,:edit,:update,:destroy]
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @employees = Employee.all
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit(:name,:breed,:age)
+    end
 end
