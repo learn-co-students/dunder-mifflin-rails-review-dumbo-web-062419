bba61c962b5096596d02213da8b37624891a7fbc
github username <github email address>
2019-07-31 18:31:51 -0400

    Done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,39 @@
 class EmployeesController < ApplicationController
+
+    def index
+      @employees = Employee.all
+    end
+
+    def show
+      @employee = Employee.find(params[:id])
+    end
+
+    def new
+      @employee = Employee.new
+      #(params.require[:employee].permit(:first_name, :last_name, :alias, :title, :office, :img_url))
+      @dogs = Dog.all
+    end
+
+    def create
+      @employee =  Employee.create(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+      redirect_to employee_path(@employee)
+    end
+
+    def edit
+      @employee = Employee.find(params[:id])
+      @dogs = Dog.all
+    end
+
+    def update
+      @employee = Employee.find(params[:id])
+      @employee.update(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+      redirect_to @employee
+    end
+
+    def destroy
+
+    end
+
+
+
 end
